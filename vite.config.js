399f954e145b7cbaccc463415507c56d9e3adf8c
rfqiw <rifqi.gusfiliandi2005@gmail.com>
2026-01-21 18:00:18 +0700

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/webgis-jabar-bencana/',
-  build: {
-    outDir: 'dist',
-    sourcemap: false
-  }
+  base: '/webgis-jabar-bencana/'
 })
